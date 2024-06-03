8b6518705e2978c9d923193765d7386a2f3c2c0e
Marko Krstic <mailme@markokrstic.com>
2024-06-03 09:33:26 +0200

    ...

--- dblocks-youtube-lazyload.php
+++ dblocks-youtube-lazyload.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Plugin Name:       DBlocks YouTube Lazyload
- * Description:       Advanced HTML Block and Code Syntax Highlighterin in one
+ * Description:       GDPR friendly lazyload youtube player that load content only if user click on the play video
  * Requires at least: 6.5.2
  * Requires PHP:      7.0
- * Version:           1.0.1
+ * Version:           1.0.0
  * Author:            DPlugins
- * * Author URI:      https://dplugins.com/
+ * Author URI:      https://dplugins.com/
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       dblocks-youtube-lazyload
@@ -27,61 +27,4 @@ define( 'DBLOCKS_YOUTUBE_LAZYLOAD_BASE',	plugin_basename(__FILE__));
 // Include the components using the prefixed path constant
 require_once DBLOCKS_YOUTUBE_LAZYLOAD_PATH . 'inc/block-registration.php';
 require_once DBLOCKS_YOUTUBE_LAZYLOAD_PATH . 'inc/category.php';
-
-
-
-// Theme API
-
-// theme-api.php
-
-add_action('rest_api_init', function () {
-    register_rest_route('dblocks-youtube-lazyload/v1', '/global-settings', [
-        'methods' => 'GET',
-        'callback' => 'get_global_settings',
-        'permission_callback' => '__return_true',
-    ]);
-
-    register_rest_route('dblocks-youtube-lazyload/v1', '/global-settings', [
-        'methods' => 'POST',
-        'callback' => 'update_global_settings',
-        'permission_callback' => function () {
-            return current_user_can('edit_posts');
-        },
-    ]);
-});
-
-function get_global_settings() {
-    return [
-        'color' => get_option('dblocks_color', '#800080'),
-        'textColor' => get_option('dblocks_textColor', '#FFFFFF'),
-        'quality' => get_option('dblocks_quality', 'maxresdefault'),
-        'playButtonSize' => get_option('dblocks_playButtonSize', '100px'),
-        'playButtonStyle' => get_option('dblocks_playButtonStyle', 0),
-        'minHeight' => get_option('dblocks_minHeight', '100px'),
-    ];
-}
-
-function update_global_settings(WP_REST_Request $request) {
-    $params = $request->get_params();
-
-    if (isset($params['color'])) {
-        update_option('dblocks_color', $params['color']);
-    }
-    if (isset($params['textColor'])) {
-        update_option('dblocks_textColor', $params['textColor']);
-    }
-    if (isset($params['quality'])) {
-        update_option('dblocks_quality', $params['quality']);
-    }
-    if (isset($params['playButtonSize'])) {
-        update_option('dblocks_playButtonSize', $params['playButtonSize']);
-    }
-    if (isset($params['playButtonStyle'])) {
-        update_option('dblocks_playButtonStyle', $params['playButtonStyle']);
-    }
-    if (isset($params['minHeight'])) {
-        update_option('dblocks_minHeight', $params['minHeight']);
-    }
-
-    return get_global_settings();
-}
+require_once DBLOCKS_YOUTUBE_LAZYLOAD_PATH . 'inc/theme-api.php';
---
+++ dev.md
@@ -0,0 +1,5 @@
+
+If you are using LocalWP open site shell and type
+```bash
+wp dist-archive "wp-content/plugins/dblocks-youtube-lazyload/"
+```
