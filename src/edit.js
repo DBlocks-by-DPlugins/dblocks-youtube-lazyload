f80a91fb6eaa40a54caab5d0776cfb978b0f6755
Marko Krstic <mailme@markokrstic.com>
2025-01-17 11:25:23 +0100

    Revert "..."
    This reverts commit 7c75b86b2bdb680bb036dd71b2ce507d8a8fcc7e.

--- src/edit.js
+++ src/edit.js
@@ -103,7 +103,6 @@ const Edit = ({ attributes, setAttributes, isSelected }) => {
     const [isEditing, setIsEditing] = useState(!url);
     const globalSettingsLoaded = useRef(false);
     const [errorMessage, setErrorMessage] = useState('');
-    const [svgContent, setSvgContent] = useState(null);
 
     const globalSettings = useSelect((select) => select(STORE_NAME).getGlobalSettings(), []);
     const { setGlobalSetting, setGlobalSettings } = useDispatch(STORE_NAME);
@@ -212,7 +211,6 @@ const Edit = ({ attributes, setAttributes, isSelected }) => {
                 playButtonStyle={playButtonStyle}
                 color={color}
                 textColor={textColor}
-                customSvgContent={svgContent}
             />
         </div>
     );
@@ -223,17 +221,6 @@ const Edit = ({ attributes, setAttributes, isSelected }) => {
             renderPreview();
         } else {
             setErrorMessage("Sorry, this content could not be embedded.");
-        }
-    };
-
-    const handleSvgDrop = (files) => {
-        const file = files[0];
-        if (file && file.type === 'image/svg+xml') {
-            const reader = new FileReader();
-            reader.onload = (event) => {
-                setSvgContent(event.target.result);
-            };
-            reader.readAsText(file);
         }
     };
 
@@ -293,16 +280,10 @@ const Edit = ({ attributes, setAttributes, isSelected }) => {
                             }}>
                             {hasDropped ? 'Dropped!' : 'Drop SVG file here'}
                             <DropZone
-                                onFilesDrop={handleSvgDrop}
+                                onFilesDrop={() => setHasDropped(true)}
                                 onHTMLDrop={() => setHasDropped(true)}
                                 onDrop={() => setHasDropped(true)}
                             />
-                            {svgContent && (
-                                <div
-                                    dangerouslySetInnerHTML={{ __html: svgContent }}
-                                    style={{ width: playButtonSize, height: playButtonSize }}
-                                />
-                            )}
                         </div>
                     )}
                     <HeightControl
