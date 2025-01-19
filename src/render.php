e0fce0cbce50aecf2e2a1d011cf48f70291b4fab
Marko Krstic <mailme@markokrstic.com>
2025-01-19 22:39:58 +0100

    Update render.php

--- src/render.php
+++ src/render.php
@@ -1,56 +1,42 @@
-<div
-    <?php
-    echo get_block_wrapper_attributes();
+<?php
+// Extract attributes with defaults
+$play_icon_color = esc_attr($attributes['textColor'] ?? '#FFFFFF');
+$play_background = esc_attr($attributes['color'] ?? '#800080');
+$playButtonSize = esc_attr($attributes['playButtonSize'] ?? '100px');
+$quality = esc_attr($attributes['quality'] ?? 'maxresdefault');
+$youtubeId = esc_attr($attributes['urlExtract'] ?? '');
+$containerId = esc_attr($attributes['containerId'] ?? '');
+$iconType = $attributes['iconType'] ?? 'iconPresets';
+$svgContent = $attributes['svgContent'] ?? '';
+$playButtonStyle = isset($attributes['playButtonStyle']) && is_numeric($attributes['playButtonStyle']) ? (int)$attributes['playButtonStyle'] : 0;
 
-    $play_icon_color = isset($attributes['textColor']) ? esc_attr($attributes['textColor']) : '#FFFFFF';
-    $play_background = isset($attributes['color']) ? esc_attr($attributes['color']) : '#800080';
-    $playButtonSize = isset($attributes['playButtonSize']) ? esc_attr($attributes['playButtonSize']) : '100px';
+// Include icons
+include 'icons.php';
 
-    $quality = isset($attributes['quality']) ? esc_attr($attributes['quality']) : 'maxresdefault';
-    $youtubeId = isset($attributes['urlExtract']) ? esc_attr($attributes['urlExtract']) : '';
+// Determine the SVG icon
+$svgIcon = $svgIcons[$playButtonStyle] ?? $svgIcons[0];
+?>
 
-    include 'icons.php';
-
-    ?>
-    id="<?php echo esc_attr($attributes['containerId']); ?>"
-
+<div <?php echo get_block_wrapper_attributes(); ?>
+    id="<?php echo $containerId; ?>"
     style="
         --play-background:<?php echo $play_background; ?>;
         --play-icon-color:<?php echo $play_icon_color; ?>;
         --play-button-size:<?php echo $playButtonSize; ?>">
 
-    <button
-        class="play-button"
-        data-youtube-id="<?php echo $youtubeId; ?>">
-
+    <button class="play-button" data-youtube-id="<?php echo esc_attr($youtubeId); ?>">
         <div
             class="play-icon-wrap play-icon-custom"
-            style="
-                width:<?php echo $playButtonSize; ?>;
-                height:<?php echo $playButtonSize; ?>">
-
-
+            style="width:<?php echo $playButtonSize; ?>; height:<?php echo $playButtonSize; ?>;">
             <?php
-            $iconType = isset($attributes['iconType']) ? $attributes['iconType'] : 'iconPresets';
-            $svgContent = isset($attributes['svgContent']) ? $attributes['svgContent'] : '';
-            $playButtonStyle = isset($attributes['playButtonStyle']) && is_numeric($attributes['playButtonStyle'])
-                ? (int) $attributes['playButtonStyle']
-                : 0;
-
-            // Ensure the index is within the bounds of the $svgIcons array
-            $svgIcon = $svgIcons[$playButtonStyle] ?? $svgIcons[0];
-
-            if ($iconType === 'custom' && !empty($svgContent)) {
-                echo $svgContent;
-            } else {
-                echo $svgIcon;
-            }
+            // Display SVG icon or custom SVG content
+            echo $iconType === 'custom' && !empty($svgContent) ? $svgContent : $svgIcon;
             ?>
         </div>
     </button>
-    <img
-        decoding="async"
+
+    <img decoding="async"
         alt="YouTube Video Placeholder"
         class="youtube-placeholder-image"
-        src="https://img.youtube.com/vi/<?php echo $youtubeId; ?>/<?php echo $quality; ?>.jpg" />
+        src="https://img.youtube.com/vi/<?php echo esc_attr($youtubeId); ?>/<?php echo esc_attr($quality); ?>.jpg" />
 </div>
