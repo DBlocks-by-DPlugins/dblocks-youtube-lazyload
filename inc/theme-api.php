973470de0de52c22215024c2c6adac4d59e4a274
Marko Krstic <mailme@markokrstic.com>
2025-01-19 09:14:21 +0100

    Update theme-api.php

--- inc/theme-api.php
+++ inc/theme-api.php
@@ -8,12 +8,12 @@ add_action('rest_api_init', function () {
     register_rest_route('dblocks-lazyload-for-youtube/v1', '/global-settings', [
         'methods' => 'GET',
         'callback' => 'dblocks_youtube_get_global_settings',
-        'permission_callback' => function(WP_REST_Request $request) {
+        'permission_callback' => function (WP_REST_Request $request) {
             $nonce = $request->get_header('X-WP-Nonce');
             if (empty($nonce) || !wp_verify_nonce($nonce, 'wp_rest') || !current_user_can('edit_posts')) {
                 return new WP_Error(
-                    'unauthorized', 
-                    'You must be logged in with proper permissions', 
+                    'unauthorized',
+                    'You must be logged in with proper permissions',
                     array('status' => 401)
                 );
             }
@@ -24,12 +24,12 @@ add_action('rest_api_init', function () {
     register_rest_route('dblocks-lazyload-for-youtube/v1', '/global-settings', [
         'methods' => 'POST',
         'callback' => 'dblocks_youtube_update_global_settings',
-        'permission_callback' => function(WP_REST_Request $request) {
+        'permission_callback' => function (WP_REST_Request $request) {
             $nonce = $request->get_header('X-WP-Nonce');
             if (empty($nonce) || !wp_verify_nonce($nonce, 'wp_rest') || !current_user_can('edit_posts')) {
                 return new WP_Error(
-                    'unauthorized', 
-                    'You must be logged in with proper permissions', 
+                    'unauthorized',
+                    'You must be logged in with proper permissions',
                     array('status' => 401)
                 );
             }
@@ -47,34 +47,29 @@ function dblocks_youtube_get_global_settings()
         'playButtonSize' => get_option('dblocks_playButtonSize', '100px'),
         'playButtonStyle' => get_option('dblocks_playButtonStyle', 0),
         'minHeight' => get_option('dblocks_minHeight', '100px'),
-        'svgContent' => get_option('dblocks_svgContent', ''), // Retrieve SVG content
+        'svgContent' => get_option('dblocks_svgContent', ''),
+        'iconType' => get_option('dblocks_iconType', 'iconType'),
     ];
 }
 
 function dblocks_youtube_update_global_settings(WP_REST_Request $request)
 {
     $params = $request->get_params();
+    $options = [
+        'color' => 'dblocks_color',
+        'textColor' => 'dblocks_textColor',
+        'quality' => 'dblocks_quality',
+        'playButtonSize' => 'dblocks_playButtonSize',
+        'playButtonStyle' => 'dblocks_playButtonStyle',
+        'minHeight' => 'dblocks_minHeight',
+        'svgContent' => 'dblocks_svgContent',
+        'iconType' => 'dblocks_iconType',
+    ];
 
-    if (isset($params['color'])) {
-        update_option('dblocks_color', $params['color']);
-    }
-    if (isset($params['textColor'])) {
-        update_option('dblocks_textColor', $params['textColor']);
-    }
-    if (isset($params['quality'])) {
-        update_option('dblocks_quality', $params['quality']);
-    }
-    if (isset($params['playButtonSize'])) {
-        update_option('dblocks_playButtonSize', $params['playButtonSize']);
-    }
-    if (isset($params['playButtonStyle'])) {
-        update_option('dblocks_playButtonStyle', $params['playButtonStyle']);
-    }
-    if (isset($params['minHeight'])) {
-        update_option('dblocks_minHeight', $params['minHeight']);
-    }
-    if (isset($params['svgContent'])) {
-        update_option('dblocks_svgContent', $params['svgContent']); // Sanitize SVG content
+    foreach ($options as $param => $option_name) {
+        if (isset($params[$param])) {
+            update_option($option_name, $params[$param]);
+        }
     }
 
     return dblocks_youtube_get_global_settings();
